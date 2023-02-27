382c207e419ed7eae7c232ee2aa03ddc6d1e4b05
sketchb02 <sketchb@uw.edu>
2023-02-27 15:22:29 -0800

    Working on structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Nav from 'Nav';
-import Footer from 'Footer';
+import Nav from './Nav.js';
+import Footer from './Footer.js';
 
-function App() {
+export default function App() {
    return (
       <div>
          <Nav />
@@ -10,5 +10,3 @@ function App() {
       </div>
    )
 }
-
-export default App;
