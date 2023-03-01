ddd8905cf49f51dbf2c67eb06fac01d12f2f6b8b
sketchb02 <sketchb@uw.edu>
2023-03-01 13:14:09 -0800

    Draft 2

--- src/components/MainMap.js
+++ src/components/MainMap.js
@@ -2,15 +2,30 @@ import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { Icon } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import marker from '../img/hiking-icon.jpeg'
 
 export default function MainMap() {
-   let coords = [47.3826, -120.4472];
+   let coords = [47.2326, -120.8472];
    let data = fetch('../../public/data/wta-parks-data.json');
+
+   const customIcon = new Icon ({
+      iconUrl: marker,
+      iconSize: [33, 33]
+   })
 
    return (
       <div id="map">
          <MapContainer center={coords} zoom={7} scrollWheelZoom={true}>
-            <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <TileLayer
+            attribution="Tiles courtesy of the <a href='https://usgs.gov/'>U.S. Geological Survey</a>"
+            url="https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}"
+            maxzoom={20}
+            />
+            <Marker position={[47.4278, -121.4135]} icon={customIcon}>
+               <Popup>
+                  PCT - Section J
+               </Popup>
+            </Marker>
          </MapContainer>
       </div>
    );
