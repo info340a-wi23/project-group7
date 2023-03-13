54909b0ed24b72f455ef08cd30867d938d091753
sketchb02 <sketchb@uw.edu>
2023-03-12 22:31:26 -0700

    Added search bar to map page, updated trails list to only display upon search

--- src/components/Map.js
+++ src/components/Map.js
@@ -20,9 +20,11 @@ function getDiffCat(diff) {
 
 export default function Map(props) {
    const [maxLength, setMaxLength] = useState(200);
-   const [diff, setDiff] = useState("Very Easy");
+   const [diff, setDiff] = useState("Any");
+   const [name, setName] = useState("");
    const [data, setData] = useState([]);
    const [bounds, setBounds] = useState(null);
+   const [searchClicked, setSearchClicked] = useState(false);
 
    useEffect(() => {
       const fetchData = async () => {
@@ -41,16 +43,32 @@ export default function Map(props) {
          const itemsWithId = valid.map((item, index) => {
          return { ...item, id: index };
       });
+
       const itemsWithDifficulties = itemsWithId.map((item) => {
          const diff = item.elevation.Gain.slice(0, -4) / (item.length.split(" ")[0] * 5280);
          const diffCat = getDiffCat(diff);
          return { ...item, diff, diffCat };
       });
-      const lengthFiltered = itemsWithDifficulties.filter(hike => hike.length.split(" ")[0] <= maxLength); // User's max length filter
-      const diffFiltered = lengthFiltered.filter(hike => hike.diffCat === diff); // User's difficulty filter
+      
+      let lengthFiltered = itemsWithDifficulties;
+      
+      if (maxLength) {
+         const maxLengthValue = parseInt(maxLength);
+         if (!isNaN(maxLengthValue)) {
+            lengthFiltered = itemsWithDifficulties.filter(hike => hike.length.split(" ")[0] <= maxLength); // User's max length filter
+         }
+      }
+
+      let diffFiltered = lengthFiltered;
+      
+      if (diff !== null) {
+         diffFiltered = lengthFiltered.filter(hike => hike.diffCat === diff); // User's difficulty filter
+      }
+
+      const nameFiltered = name ? diffFiltered.filter(hike => hike.name.toLowerCase().includes(name.toLowerCase())) : diffFiltered; // User's hike name filter
 
       if (bounds !== null) {
-         const boundsFiltered = diffFiltered.filter(hike =>
+         const boundsFiltered = nameFiltered.filter(hike =>
             hike.coordinates.lat >= bounds._southWest.lat &&
             hike.coordinates.lat <= bounds._northEast.lat &&
             hike.coordinates.lon >= bounds._southWest.lng &&
@@ -58,7 +76,7 @@ export default function Map(props) {
          );
          return boundsFiltered;
       } else {
-         return lengthFiltered;
+         return diffFiltered;
       }
    }
 
@@ -67,8 +85,14 @@ export default function Map(props) {
       const maxLengthInput = document.getElementById('max-length').value;
       setMaxLength(parseInt(maxLengthInput));
       const diffInput = document.getElementById('diff').value.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
-      setDiff(diffInput);
-      console.log(diffInput);
+      if (diffInput === 'Any') {
+         setDiff(null);
+      } else {
+         setDiff(diffInput);
+      }
+      const nameInput = document.getElementById('name').value;
+      setName(nameInput);
+      setSearchClicked(true);
    };
 
    const handleBoundsChanged = (newBounds) => {
@@ -85,32 +109,39 @@ export default function Map(props) {
                <input type="text" id="max-length" name="max-length" placeholder="Max length (mi)..." />
                <label htmlFor="diff">Difficulty:</label>
                <select type="diff" id="diff" name="diff">
+                  <option value="any">Any</option>
                   <option value="very-easy">Very Easy</option>
                   <option value="easy">Easy</option>
                   <option value="medium">Medium</option>
                   <option value="hard">Hard</option>
                   <option value="very-hard">Very Hard</option>
                </select>
-               <input id="submit" type="submit" value="Search"></input>
+               <label htmlFor="name">Hike Name:</label>
+               <input type="text" id="name" name="name" placeholder="Name..." />
+               <input id="submit" type="submit" value="Search" />
             </form>
             <MainMap center={[47.2326, -120.8472]} zoom={7} maxLength={maxLength} data={filteredData(data)} onBoundsChanged={handleBoundsChanged} />
-            <h2>Trails</h2>
-            <div className="container">
-               {filteredData(data).map(item => {
-                  return (
-                     <div className="row trail-card" key={item.id}>
-                        <div className="col col-12">
-                           <div className="card map-list">
-                              <h5 className="card-title"><Link to={item.url.split("/")[item.url.split("/").length - 1]}>{item.name}</Link></h5>
-                              <h6 className="card-subtitle">Length: {item.length} {' \u2022 '} Difficulty: {item.diffCat}</h6>
-                              <p><strong>Elevation Gain:</strong> {item.elevation.Gain}</p>
-                              <p><strong>Features:</strong> {item.features.map(feature => feature).join(' \u2022 ')}</p>
+            {searchClicked && (
+               <div>
+                  <h2>Trails</h2>
+                  <div className="container">
+                     {filteredData(data).map(item => {
+                        return (
+                           <div className="row trail-card" key={item.id}>
+                              <div className="col col-12">
+                                 <div className="card map-list">
+                                    <h5 className="card-title"><Link to={item.url.split("/")[item.url.split("/").length - 1]}>{item.name}</Link></h5>
+                                    <h6 className="card-subtitle">Length: {item.length} {' \u2022 '} Difficulty: {item.diffCat}</h6>
+                                    <p><strong>Elevation Gain:</strong> {item.elevation.Gain}</p>
+                                    <p><strong>Features:</strong> {item.features.map(feature => feature).join(' \u2022 ')}</p>
+                                 </div>
+                              </div>
                            </div>
-                        </div>
-                     </div>
-                  );
-               })}
-            </div>
+                        );
+                     })}
+                  </div>
+               </div>
+            )}
          </main>
          <Footer />
       </div>
