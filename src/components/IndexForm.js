2efa14c6971a27254fe2718039790943b6d9355a
sketchb02 <sketchb@uw.edu>
2023-03-13 19:25:51 -0700

    Minor changes to address draft problems

--- src/components/IndexForm.js
+++ src/components/IndexForm.js
@@ -3,17 +3,20 @@ import React from 'react';
 export default function IndexForm(props) {
    return (
       <form>
-         <label htmlFor="name">Name:</label>
-         <br />
-         <input type="text" id="name" name="name" placeholder="Your Name..." />
-         <br />
-         <label htmlFor="email">Email:</label>
-         <br />
-         <input type="email" id="email" name="email" placeholder="Your Email..." />
-         <br />
-         <textarea id="body" name="body" placeholder="Your Message..."></textarea>
-         <br />
-         <input type="submit" value="Submit"></input>
+         <div style={{ display: "block" }}>
+            <label htmlFor="name" style={{ paddingRight: "0.5rem" }}>Name:</label>
+            <input type="text" id="name" name="name" placeholder="Your Name..." />
+         </div>
+         <div style={{ display: "block", marginTop: "0.5rem" }}>
+            <label htmlFor="email" style={{ paddingRight: "0.5rem" }}>Email:</label>
+            <input type="email" id="email" name="email" placeholder="Your Email..." />
+         </div>
+         <div style={{ display: "block" }}>
+            <textarea id="body" name="body" placeholder="Your Message..."></textarea>
+         </div>
+         <div style={{ display: "inline-block", marginTop: "0.5rem" }}>
+            <input type="submit" value="Submit"></input>
+         </div>
       </form>
    );
 }
