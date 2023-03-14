a29fd259c69537d7dde68652b33f49ec192ea16a
seang25 <seang25@uw.edu>
2023-03-13 20:36:05 -0700

    fixes

--- src/components/Equipment.js
+++ src/components/Equipment.js
@@ -5,7 +5,7 @@ const cards = [
     {img: 'boots.jpeg', p: 'Hiking boots are crucial for comfort and stability on the trail.'}, {img: 'backpack.jpeg', p: 'A reliable backpack is crucial for carrying all of your gear and supplies.'}, 
     {img: 'bottle.jpeg', p: 'Staying hydrated is key on a hiking trip, so bring a water bottle!'}, {img: 'compass.jpeg', p: 'Dont rely on technology alone! Always bring a map and compass as backup.'},
     {img: 'hat.jpeg', p: 'Protect yourself from the sun with sunscreen and a hat.'}, {img: 'aid.jpeg', p: 'Be prepared for emergencies with a well-stocked first aid kit.'}, 
-    {img: 'light.jpeg', p: 'Dont get caught in the dark! Always bring a flashlight or headlamp.'}, {img: 'snacks.jpeg', p: 'Keep your energy up with snacks and meals that are easy to pack and eat on the trail.'},
+    {img: 'light.jpeg', p: 'Dont get caught in the dark! Always bring a flashlight or headlamp.'}, {img: 'snacks.jpeg', p: 'Keep your energy up with snacks that are easy to pack and eat on the trail.'},
 ]
 
 export default function Equipment(props) {
